fda9f72aef641acb48f51f9d69705c3b387a9195
Michael Equi <equim20@student.jhs.net>
2018-07-09 19:54:05 -0700

    added a fully functional non-PID vector drive. The temporary param server variables must be changed out for copilot control topic as soon as it is created

--- ros_workspace/src/rov_control_interface/src/logi_joystick.cpp
+++ ros_workspace/src/rov_control_interface/src/logi_joystick.cpp
@@ -25,36 +25,86 @@ class logitechExtreme3DJoystick
     //doubles for holding the value of the control axis
     double a_axis, l_axisLR, l_axisFB, v_axis;
 
-    bool thrustEN; //thrusters enabled
+    bool thrustEN; //thrusters enabled?
 
     //inversion added here
     //inversion -> 1 Front, 2 Left, 3 Back, 4 Right
     int inversion;
 
+    //variables for bilinear control
+    const double bilinearRatio;
+    const double bilinearThreshold;
+
     //subscribing to the logitech joystick and outputting control vectors
     ros::Publisher vel_pub;
     ros::Subscriber joy_sub;
+
+    void bilinearCalc(double &axis){
+        if((bilinearThreshold * -0.32768)<= axis && axis < (bilinearThreshold * 0.32767)){ //middle range
+            axis/=bilinearRatio;
+
+        } else if((bilinearThreshold * 0.32767) < axis && axis <= 0.32767) { //upper range
+            axis = (bilinearRatio * axis) + ((bilinearRatio * -0.32767) + 0.32767);
+
+        } else if(-0.32768 <= axis && axis < (bilinearThreshold * -0.32768)){//lower range
+            axis = (bilinearRatio * axis) + ((bilinearRatio * 0.32768) - 0.32768);
+
+        }
+    }
 
 
     //What the node does when joy_sub receives a new publish message
     void joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
 
-        //check if thrusters disabled
-        if (n.param("thrustersEnabled", thrustEN, false) && thrustEN == true){
+
+        //once copilot interface is created the params will be replaced with topics (inversion + sensiitivity)
+
+        //check if thrusters disabled (temporary until addition of a dynamic_reconfigure)
+        if (n.param("thrustersEnabled", thrustEN, false) && thrustEN == true) {
 
             //joystick message
             //float32[] axes          the axes measurements from a joystick
             //int32[] buttons         the buttons measurements from a joystick
 
             //temporary implementation of scale
-            l_scale = 1;
-            a_scale = 1;
-            v_scale = 1;
+            n.param("linearSensitivity", l_scale, 0.75);
+            n.param("rotationalSensitivity", a_scale, 0.75);
+            n.param("verticalSensitivity", v_scale, 0.75);
 
-            a_axis = joy->axes[angularAxisIndex]*a_scale;
-            l_axisLR = joy->axes[linearAxisLRIndex]*l_scale;
-            l_axisFB = joy->axes[linearAxisFBIndex]*l_scale;
-            v_axis = joy->axes[verticalAxisIndex]*v_scale;
+            //store axes variables and handle 4 cases of inversion
+            a_axis = joy->axes[angularAxisIndex] * a_scale;
+            v_axis = joy->axes[verticalAxisIndex] * v_scale;
+
+            n.param("inversion", inversion, 0); //get the value of the inversion param (temporary)
+
+            switch (inversion){
+            case 1 : //left side is front
+                l_axisFB = joy->axes[linearAxisLRIndex] * l_scale;
+                l_axisLR = joy->axes[linearAxisFBIndex] * l_scale;
+                break;
+            case 2 : //back side is front
+                l_axisLR = joy->axes[linearAxisLRIndex] * l_scale * -1;
+                l_axisFB = joy->axes[linearAxisFBIndex] * l_scale * -1;
+                break;
+            case 3 : //right side is front
+                l_axisFB = joy->axes[linearAxisLRIndex] * l_scale * -1;
+                l_axisLR = joy->axes[linearAxisFBIndex] * l_scale * -1;
+                break;
+            default: //front side is front
+                l_axisLR = joy->axes[linearAxisLRIndex] * l_scale;
+                l_axisFB = joy->axes[linearAxisFBIndex] * l_scale;
+                break;
+            }
+
+
+
+            //apply the bilinear ratio on all axis
+            bilinearCalc(a_axis);
+            bilinearCalc(l_axisLR);
+            bilinearCalc(l_axisFB);
+            bilinearCalc(v_axis);
+
+
 
         } else {
 
@@ -65,7 +115,7 @@ class logitechExtreme3DJoystick
 
         }
 
-        //publish the vector values
+        //publish the vector values -> build up command vector message
         geometry_msgs::Twist commandVectors;
 
         commandVectors.linear.x = l_axisLR;
@@ -84,7 +134,8 @@ class logitechExtreme3DJoystick
 
 public:
     //constructor
-    logitechExtreme3DJoystick()
+    logitechExtreme3DJoystick(): bilinearRatio(1.5), bilinearThreshold(1.0 / bilinearRatio), inversion(0),
+                                 thrustEN(false)
     {
         vel_pub = n.advertise<geometry_msgs::Twist>("rov/cmd_vel", 1);
         joy_sub = n.subscribe<sensor_msgs::Joy>("joy", 2, &logitechExtreme3DJoystick::joyCallback, this);
