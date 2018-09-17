6b55da4fe57b2d2046eeba28698bd37719ee147f
Caelin Sutch <sutchc20@student.jhs.net>
2018-09-17 12:11:32 -0700

    Added other modules

---
+++ copilot/src/app/Modules/secondary-camera-module/secondary-camera-module.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secondary-camera-module',
+  templateUrl: './secondary-camera-module.component.html',
+  styleUrls: ['./secondary-camera-module.component.css']
+})
+export class SecondaryCameraModuleComponent {
+
+  cameras = ['Camera 1', 'Camera 2', 'Camera 3', 'Camera 4', 'Camera 5', 'Camera 6', 'Camera 7', 'Camera 8', 'Camera 9'];
+
+}
