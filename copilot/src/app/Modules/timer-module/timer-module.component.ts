847b3ccafece425e13ea14dc6f2420b35696598d
Caelin Sutch <sutchc20@student.jhs.net>
2018-09-18 11:45:20 -0700

    Timer Module/Other Fixes

---
+++ copilot/src/app/Modules/timer-module/timer-module.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timer-module',
+  templateUrl: './timer-module.component.html',
+  styleUrls: ['./timer-module.component.css']
+})
+export class TimerModuleComponent {
+
+  time = '0:00';
+
+}
