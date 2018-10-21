2955f21f2878d743a8bff326a316791eb2ed3072
Caelin Sutch <sutchc20@student.jhs.net>
2018-10-21 10:37:15 -0700

    Accidently had been developing in ROVTESTBENCH

--- copilot/src/app/CopilotModules/timer-module/readme.md
+++
@@ -1,7 +0,0 @@
-## Some notes
-In the code I choose to make a stopwatch using the: 
-``setInterval`` notation. However, this is not the most
-accurate, as you may see. I started developing a more precise
-way to count time by using system time, 
-but I never fully implemented it. If you would like to finish
-it, feel free. 
--- copilot/src/app/copilot/copilot.component.ts
+++ copilot/src/app/copilot/copilot.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
 
-@Component({selector: 'app-hello-world', template: 'Hello World!'})
-  class HelloWorldComponent {
-}
-
 @Component({
   selector: 'app-copilot',
   templateUrl: './copilot.component.html',
   styleUrls: ['./copilot.component.css']
 })
 
-export class CopilotComponent {
-  HelloWorld = HelloWorldComponent;
-}
+export class CopilotComponent { }
