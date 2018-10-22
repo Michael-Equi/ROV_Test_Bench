f9d8ab1ea55e5309808d8701ae43853ada4a9fd7
Ubuntu16 <caelinsutch@gmail.com>
2018-10-22 16:38:11 -0700

    Connection made!

--- copilot/src/app/app.component.ts
+++ copilot/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { RoslibService } from './roslib.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // The constructor takes the roslibservice import statement and assigns it
+  // Done before onInit
+  constructor(private roslibservice: RoslibService) { }
+  // OnInit initializes the Roslib service, and gets data to test connection
+  ngOnInit() {
+    this.roslibservice.initialize();
+    this.roslibservice.getDriveControlData();
+  }
+
   title = 'materialdashboard';
 }
