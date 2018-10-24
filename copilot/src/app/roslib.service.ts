081d884486567a563dea03a550fbf15b2d003c26
Ubuntu16 <caelinsutch@gmail.com>
2018-10-23 18:47:31 -0700

    Got Service More or Less Working

--- copilot/src/app/roslib.service.ts
+++ copilot/src/app/roslib.service.ts
@@ -1,18 +1,24 @@
 import {Injectable} from '@angular/core';
-import { Observable } from 'rxjs';
-import  '../assets/roslib.js';
+import {BehaviorSubject, Observable} from 'rxjs';
+import '../assets/roslib.js';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class RoslibService {
-  ros = new ROSLIB.Ros({
+  // Creates object with the ROS library
+  // @ts-ignore <= Makes ts happy, wont error
+    ros = new ROSLIB.Ros({
+      // Set listen URL for ROS communication
     url : 'ws://localhost:9090'
   });
-
-  data;
-
+  // Data variable to hold ROS data
+  data: BehaviorSubject<any> = new BehaviorSubject('Untouched');
+  // Initialize function sets everything up, called on a ngOnInit in app.component
   initialize() {
+    let data = "UnTouch";
+    // Listens for error from ROS and logs it
     this.ros.on('error', function(error) {
       console.log(error);
     });
@@ -21,26 +27,27 @@ export class RoslibService {
     this.ros.on('connection', function() {
       console.log('Connection made!');
     });
-
+    // Logs when connection is closed
     this.ros.on('close', function() {
       console.log('Connection closed.');
     });
 
     // Get Data from ROS
-    const driveControlListener = new ROSLIB.Topic({
-      ros : this.ros,
-      name : '/drive_control/parameter_updates',
-      messageType : 'dynamic_reconfigure/Config'
+    // @ts-ignore
+      const driveControlListener = new ROSLIB.Topic({
+      ros : this.ros, // Points to ROS variable
+      name : '/drive_control/parameter_updates', // Topic Name
+      messageType : 'dynamic_reconfigure/Config' // Message Type
     });
 
     // Subscribe to ROS data
-    driveControlListener.subscribe(function(message) {
-      console.log('Recieved Message on ' + driveControlListener.name + ' : ' + message);
-      this.data = message;
+     driveControlListener.subscribe((message) => {
+      console.log('Recieved Message on ' + driveControlListener.name + ' : ' + message.bools);
+      this.data.next(message);
     });
   }
 
   getDriveControlData(): Observable<any> {
-    return this.data;
+    return this.data.asObservable();
   }
 }
