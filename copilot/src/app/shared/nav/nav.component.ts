890cc390bf03991f05ecdcf2990589e7942c9b29
Ubuntu16 <caelinsutch@gmail.com>
2018-10-24 19:47:23 -0700

    Fixed links, added bmp280 and bno055 service

--- copilot/src/app/shared/nav/nav.component.ts
+++ copilot/src/app/shared/nav/nav.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
   icons = [
-      {src: '/src/assets/House(Unselected).svg', clickedsrc: '/src/assets/House(Selected).svg', selected: false, link: '/copilot'},
-      {src: '/src/assets/Bolt(Unselected).svg', clickedsrc: '/src/assets/Bolt(Selected).svg', selected: false, link: '/drq'},
-      {src: '/src/assets/CircuitBoard(Unselected).svg', clickedsrc: '/src/assets/CircuitBoard(Selected).svg', selected: false, link: '/sensortelemetry'},
-      {src: '/src/assets/Settings(Unselected).svg', clickedsrc: '/src/assets/Settings(Selected).svg', selected: false}
+      {src: '../../../assets/House(Unselected).svg', clickedsrc: '../../../assets/House(Selected).svg', selected: false, link: '/copilot'},
+      {src: '../../../assets/Bolt(Unselected).svg', clickedsrc: '../../../assets/Bolt(Selected).svg', selected: false, link: '/drq'},
+      {src: '../../../assets/CircuitBoard(Unselected).svg', clickedsrc: '../../../assets/CircuitBoard(Selected).svg', selected: false, link: '/sensortelemetry'},
+      {src: '../../../assets/Settings(Unselected).svg', clickedsrc: '../../../assets/Settings(Selected).svg', selected: false}
   ];
 
   selected(icon) {
