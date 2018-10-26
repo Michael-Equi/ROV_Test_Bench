04552dae718ec0c5fc2b3365ac06e70a10f1b58b
Ubuntu16 <caelinsutch@gmail.com>
2018-10-26 15:37:58 -0700

    Ugh, left in broken state, idk how to fix

--- copilot/src/app/copilot/copilot-components/depth-chart/depth-chart.component.ts
+++ copilot/src/app/copilot/copilot-components/depth-chart/depth-chart.component.ts
@@ -1,4 +1,4 @@
-import {OnInit, Component, AfterViewInit} from '@angular/core';
+import {OnInit, Component, AfterViewInit, ViewChild} from '@angular/core';
 import { Chart } from 'chart.js';
 import { Ms5837Service } from '../../../services/ms5837.service';
 import { Ms5837Data } from '../../../services/data-models/ms5837.model';
@@ -8,49 +8,21 @@ import { Ms5837Data } from '../../../services/data-models/ms5837.model';
   templateUrl: './depth-chart.component.html',
   styleUrls: ['./depth-chart.component.css']
 })
+
 export class DepthChartComponent implements OnInit {
-    exteriorAltitude = [0];
-    seconds = [0];
-    name = 'Depth Chart';
-    type = 'line';
-    data = {
-        labels: this.seconds,
-        datasets: [{
-            label: 'Depth',
-            data: this.exteriorAltitude,
-            backgroundColor: [
-                'rgba(0,188,212, .3)'
-            ],
-            borderColor: [
-                '#00bcd4'
-            ],
-            borderWidth: 1
-            }]
-    };
-    options = {
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    };
+    exteriorAltitude = [];
+    seconds = [];
+
     constructor(private ms5837Service: Ms5837Service) {}
     ngOnInit() {
         this.ms5837Service.initialize();
         this.ms5837Service.getData().subscribe((msg: Ms5837Data) => {
             if (msg !== undefined) {
-              console.log(msg);
-              console.log(msg.altitudeM);
-              this.exteriorAltitude.push(msg.altitudeM);
-              console.log(this.exteriorAltitude);
-              // console.log(msg.header.stamp.secs);
-              // this.seconds.push(msg.header.stamp.secs);
+              // console.log(msg);
+              // this.data.datasets[0].data.push(msg.altitudeM);
+              // console.log(this.data.datasets[0].data);
+              // this.data.labels.push(msg.header.stamp.secs);
+              // console.log(this.data.labels);
             }
         });
     }
