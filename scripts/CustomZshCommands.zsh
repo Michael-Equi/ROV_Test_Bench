044956338d0558e984b685df5296a00bbc8524b5
Jaiveer <gahuniaj20@student.jhs.net>
2018-10-27 18:52:58 -0700

    add zsh support for custom commands scripts fix typo in CustomCommands.sh

--- scripts/CustomZshCommands.zsh
+++ scripts/CustomZshCommands.zsh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/zsh
 
 function clion(){
    path=$(find / -name "clion.sh")
@@ -7,11 +7,11 @@ function clion(){
    while [ $continue != "t" ];
    do
       if [ ! -z $(sudo find "${ROSWorkspace}" -name "devel")  ]; then
-         source "${ROSWorkspace}/devel/setup.bash"
-         echo "${ROSWorkspace}/devel/setup.bash successfully sourced! "
+         source "${ROSWorkspace}/devel/setup.zsh"
+         echo "${ROSWorkspace}/devel/setup.zsh successfully sourced! "
          continue="t"
       else
-         read -p "Source of ${ROSWorkspace}/devel/setup.bash failed do you wish to continue (t/f)? " continue
+         read -p "Source of ${ROSWorkspace}/devel/setup.zsh failed do you wish to continue (t/f)? " continue
          if [ $continue != "t" ]; then
             read -p "Enter the ROS package you would like to source: " ROSWorkspace
          fi
@@ -29,11 +29,11 @@ function webstorm(){
    while [ $continue != "t" ];
    do
       if [ ! -z $(sudo find "${ROSWorkspace}" -name "devel")  ]; then
-         source "${ROSWorkspace}/devel/setup.bash"
-         echo "${ROSWorkspace}/devel/setup.bash successfully sourced! "
+         source "${ROSWorkspace}/devel/setup.zsh"
+         echo "${ROSWorkspace}/devel/setup.zsh successfully sourced! "
          continue="t"
       else
-         read -p "Source of ${ROSWorkspace}/devel/setup.bash failed do you wish to continue (t/f)? " continue
+         read -p "Source of ${ROSWorkspace}/devel/setup.zsh failed do you wish to continue (t/f)? " continue
          if [ $continue != "t" ]; then
             read -p "Enter the ROS package you would like to source: " ROSWorkspace
          fi
@@ -51,11 +51,11 @@ function pycharm(){
    while [ $continue != "t" ];
    do
       if [ ! -z $(sudo find "${ROSWorkspace}" -name "devel")  ]; then
-         source "${ROSWorkspace}/devel/setup.bash"
-         echo "${ROSWorkspace}/devel/setup.bash successfully sourced! "
+         source "${ROSWorkspace}/devel/setup.zsh"
+         echo "${ROSWorkspace}/devel/setup.zsh successfully sourced! "
          continue="t"
       else
-         read -p "Source of ${ROSWorkspace}/devel/setup.bash failed do you wish to continue (t/f)? " continue
+         read -p "Source of ${ROSWorkspace}/devel/setup.zsh failed do you wish to continue (t/f)? " continue
          if [ $continue != "t" ]; then
             read -p "Enter the ROS package you would like to source: " ROSWorkspace
          fi
