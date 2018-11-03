a048abeaaf23e3676286169d97002dda5143d8e4
Unknown <parkera21@student.jhs.net>
2018-11-03 10:05:26 -0700

    Touchscreen Update

--- touchscreen/src/app/app.module.ts
+++ touchscreen/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule, MatCardModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
