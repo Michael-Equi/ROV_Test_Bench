ecf4b56cf0086f63e7b8bbf9d0cc15965e2f0466
Unknown <parkera21@student.jhs.net>
2018-11-03 15:01:22 -0700

    Update Depth Module

--- copilot/src/app/copilot/copilot-components/depth-chart/depth-chart.component.ts
+++ copilot/src/app/copilot/copilot-components/depth-chart/depth-chart.component.ts
@@ -10,20 +10,20 @@ import { Ms5837Data } from '../../../services/data-models/ms5837.model';
 })
 
 export class DepthChartComponent implements OnInit {
-    exteriorAltitude = [];
-    seconds = [];
+    rovDepth: number; // ROV Depth in Meters
 
     constructor(private ms5837Service: Ms5837Service) {}
     ngOnInit() {
         this.ms5837Service.initialize();
         this.ms5837Service.getData().subscribe((msg: Ms5837Data) => {
-            if (msg !== undefined) {
-              // console.log(msg);
-              // this.data.datasets[0].data.push(msg.altitudeM);
-              // console.log(this.data.datasets[0].data);
-              // this.data.labels.push(msg.header.stamp.secs);
-              // console.log(this.data.labels);
-            }
+            this.rovDepth = msg.depth;
+            // if (msg !== undefined) {
+            //   // console.log(msg);
+            //   // this.data.datasets[0].data.push(msg.altitudeM);
+            //   // console.log(this.data.datasets[0].data);
+            //   // this.data.labels.push(msg.header.stamp.secs);
+            //   // console.log(this.data.labels);
+            // }
         });
     }
 }
