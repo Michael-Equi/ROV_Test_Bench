04b77fe8ca42f4203938e8cedf7ec37a94aa5831
Unknown <parkera21@student.jhs.net>
2018-11-17 14:49:02 -0800

    Touchscreen slider update (WORKING)

--- ros_workspace/src/tcu_touchscreen/src/src/app/sliders/sliders.component.ts
+++ ros_workspace/src/tcu_touchscreen/src/src/app/sliders/sliders.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Options } from 'ng5-slider';
 
 @Component({
   selector: 'app-sliders',
   templateUrl: './sliders.component.html',
   styleUrls: ['./sliders.component.css']
 })
-export class SlidersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class SlidersComponent {
+    value: number = 0;
+    value1: number = 0;
+    valuemax: number = 0;
+    valuemax1: number = 0;
+    options: Options = {
+        floor: 0,
+        ceil: 40
+    }
+    options2: Options = {
+        floor: 0,
+        ceil: 50
+    };
 }
