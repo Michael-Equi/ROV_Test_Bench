7bffe003380c4a80ada45d9a8d5b3d79553a6d04
Unknown <parkera21@student.jhs.net>
2018-11-19 14:11:02 -0800

    Broken Power Button

--- ros_workspace/src/tcu_touchscreen/src/src/app/confirmdialog/confirmdialog.component.ts
+++ ros_workspace/src/tcu_touchscreen/src/src/app/confirmdialog/confirmdialog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-confirmdialog',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./confirmdialog.component.css']
 })
 export class ConfirmdialogComponent {
+
+    constructor(
+        private dialogRef: MatDialogRef<ConfirmdialogComponent>
+    ) {}
 
     confirm() {
        console.log('Confirm');
