09e49cd8c38a69988d6d441fab4e95bcb753310d
Caelin Sutch <caelinsutch@gmail.com>
2018-11-19 17:04:09 -0800

    Added quaternion support

--- ros_workspace/src/sensor_readout/src/talker.py
+++ ros_workspace/src/sensor_readout/src/talker.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
+
 import rospy
-from sense_hat import SenseHat
 import time
+import math
+from sense_hat import SenseHat
 from std_msgs.msg import Int64
+from std_msgs.msg import Float64
+#from geometry_msgs.msg import Quaternion
+#from tf.transformations import quaternion_from_euler 
 
 sense = SenseHat()
 
@@ -11,13 +16,11 @@ def talker():
 	pressure_pub = rospy.Publisher('rov/pressure', Int64, queue_size = 3) 
 	humidity_pub = rospy.Publisher('rov/humidity', Int64, queue_size = 3)
 
-	yaw_pub = rospy.Publisher('rov/yaw', Int64, queue_size = 3) #yaw, pitch and roll
-	pitch_pub = rospy.Publisher('rov/pitch', Int64, queue_size = 3)
-	roll_pub = rospy.Publisher('rov/roll', Int64, queue_size = 3)
+	#rotation_pub = rospy.Publisher("rov/rotation", Quaternion, queue_size = 3) #rotation
 
-	x_direction_pub = rospy.Publisher('rov/Xacceleration', Int64, queue_size = 3)#X, y and z with speed
-	y_direction_pub = rospy.Publisher('rov/Yacceleration', Int64, queue_size = 3)
-	z_direction_pub = rospy.Publisher('rov/Zacceleration', Int64, queue_size = 3)
+	x_direction_pub = rospy.Publisher('rov/Xacceleration', Float64, queue_size = 3)#X, y and z with speed
+	y_direction_pub = rospy.Publisher('rov/Yacceleration', Float64, queue_size = 3)
+	z_direction_pub = rospy.Publisher('rov/Zacceleration', Float64, queue_size = 3)
 
 	rospy.init_node('sensor_readout')
 	rate = rospy.Rate(60)
@@ -27,9 +30,7 @@ def talker():
 		humidity_pub.publish(round(sense.get_humidity()))#Temperature pressure humidity
 
 		orientation = sense.get_orientation()#roll pitch and yaw
-		yaw_pub.publish(round(orientation["yaw"]))
-		pitch_pub.publish(round(orientation["pitch"]))
-		roll_pub.publish(round(orientation["roll"]))
+	#	rotation_pub.publish(quaternion_from_euler(radians(orientation["yaw"]), radians(orientation["pitch"]), radians(orientation["roll"])))	#converts the degrees returned by get_orientation() to radians then uses all 4 directions into a quaternion, then publishes it
 
 		acceleration = sense.get_accelerometer_raw()#x y and z G force, not rounded
 		x_direction_pub.publish(acceleration['x'])
