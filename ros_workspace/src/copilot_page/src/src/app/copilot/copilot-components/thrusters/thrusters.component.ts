67965ca7e0b4982fe76aab752fb094875d822610
Unknown <parkera21@student.jhs.net>
2018-12-08 08:35:48 -0800

    ROS Integration with dials

--- ros_workspace/src/copilot_page/src/src/app/copilot/copilot-components/thrusters/thrusters.component.ts
+++ ros_workspace/src/copilot_page/src/src/app/copilot/copilot-components/thrusters/thrusters.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import 'jquery-knob';
+import { HorizontalDriveService } from '../../../services/subscribers/horizontal-drive.service';
+import { HorizontalDriveModel } from '../../../services/data-models/horizontal-drive.model';
+import { VerticalDriveService } from '../../../services/subscribers/vertical-drive.service';
+import { VerticalDriveModel } from '../../../services/data-models/vertical-drive.model';
+import {Bmp280Model} from "../../../services/data-models/bmp280.model";
+import {Sht31Data} from "../../../services/data-models/sht31.model";
 
 @Component({
   selector: 'app-thrusters',
@@ -10,12 +16,14 @@ import 'jquery-knob';
 export class ThrustersComponent implements OnInit {
     name = 'ROV Thrusters';
 
-    thruster1 = 0;
-    thruster2 = 0;
-    thruster3 = 0;
-    thruster4 = 0;
-    thruster5 = 0;
-    thruster6 = 0;
+    thruster1: number;
+    thruster2: number;
+    thruster3: number;
+    thruster4: number;
+    thruster5: number;
+    thruster6: number;
+
+    constructor(private horizontaldriveService: HorizontalDriveService, private verticaldriveService: VerticalDriveService) {}
 
     ngOnInit() {
         $(".dial").knob({
@@ -28,8 +36,38 @@ export class ThrustersComponent implements OnInit {
             'fgColor': '#4287e0',
             'bgColor': '#204d7a'
         });
+        this.horizontaldriveService.initialize();
+        this.horizontaldriveService.getData().subscribe((msg: HorizontalDriveModel) => {
+            this.thruster1 = msg.t1;
+            this.thruster2 = msg.t2;
+            this.thruster3 = msg.t3;
+            this.thruster4 = msg.t4;
+            $('#thruster1')
+                .val(this.thruster1)
+                .trigger('change');
+            $('#thruster2')
+                .val(this.thruster2)
+                .trigger('change');
+            $('#thruster3')
+                .val(this.thruster3)
+                .trigger('change');
+            $('#thruster4')
+                .val(this.thruster4)
+                .trigger('change');
+        });
+        this.verticaldriveService.initialize();
+        this.verticaldriveService.getData().subscribe((msg: VerticalDriveModel) => {
+            this.thruster5 = msg.t1;
+            this.thruster6 = msg.t2;
+            $('#thruster5')
+                .val(this.thruster5)
+                .trigger('change');
+            $('#thruster6')
+                .val(this.thruster6)
+                .trigger('change');
+        });
     }
-    
+
 
 
 }
