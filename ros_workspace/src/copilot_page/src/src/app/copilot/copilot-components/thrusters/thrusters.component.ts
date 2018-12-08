0ba82ba056d276427f8ab68a703f2a4a9df6382f
Unknown <parkera21@student.jhs.net>
2018-12-07 19:58:33 -0800

    Thruster dials integrated and jQuery added

--- ros_workspace/src/copilot_page/src/src/app/copilot/copilot-components/thrusters/thrusters.component.ts
+++ ros_workspace/src/copilot_page/src/src/app/copilot/copilot-components/thrusters/thrusters.component.ts
@@ -1,11 +1,35 @@
-import {Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'jquery-knob';
 
 @Component({
   selector: 'app-thrusters',
   templateUrl: './thrusters.component.html',
   styleUrls: ['./thrusters.component.css']
 })
-export class ThrustersComponent {
+export class ThrustersComponent implements OnInit {
     name = 'ROV Thrusters';
+
+    thruster1 = 0;
+    thruster2 = 0;
+    thruster3 = 0;
+    thruster4 = 0;
+    thruster5 = 0;
+    thruster6 = 0;
+
+    ngOnInit() {
+        $(".dial").knob({
+            'min': 0,
+            'max': 100,
+            'width': '100%',
+            'height': '100%',
+            'lineCap': 'round',
+            'readOnly': true,
+            'fgColor': '#4287e0',
+            'bgColor': '#204d7a'
+        });
+    }
+    
+
 
 }
