affa6f1bc3a035d8c47c4b9afe1f1ef524195e3c
Alden Parker <parkera21@student.jhs.net>
2018-12-08 14:42:05 -0800

    Thrusters and Snackbar

--- ros_workspace/src/copilot_page/src/src/app/copilot/copilot.component.ts
+++ ros_workspace/src/copilot_page/src/src/app/copilot/copilot.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {fromEvent} from 'rxjs';
-import {ThrustersStatusService} from '../services/publishers/thrusters-status.service';
-import {MatSnackBar} from '@angular/material';
+
 
 @Component({
     selector: 'app-copilot',
@@ -9,36 +7,7 @@ import {MatSnackBar} from '@angular/material';
     styleUrls: ['./copilot.component.css']
 })
 
-export class CopilotComponent implements OnInit {
-
-    // Declares variable to hold thruster status (enabled/disabled)
-    thrusterStatus: boolean;
-
-
-    constructor(private thrusterStatusService: ThrustersStatusService, public thrusterNotification: MatSnackBar) {
-    }
-
-    //  Runs on key press
-    keyPress(character) {
-        if (character.code = 'keyE') {
-            // Changes thruster status
-            this.thrusterStatus = !this.thrusterStatus;
-            this.thrusterStatusService.publish(this.thrusterStatus);
-            // Opens snackbar (that's the real name) that displays thruster status)
-            this.thrusterNotification.open(
-                this.thrusterStatus ? 'Thrusters Enabled' : 'Thrusters Disabled',
-                'Exit'
-            );
-        } else {
-            console.log(character.code);
-        }
-    }
-
-    ngOnInit() {
-        // Creates and subscribes too observable that listens for keypresses, runs keypress function as callback
-        fromEvent(document, 'keyup').pipe().subscribe(character => this.keyPress(character));
-        this.thrusterStatusService.initialize();
-    }
+export class CopilotComponent {
 
 
 }
--- ros_workspace/src/copilot_page/src/src/app/shared/nav/nav.component.ts
+++ ros_workspace/src/copilot_page/src/src/app/shared/nav/nav.component.ts
@@ -1,13 +1,15 @@
 import {Component, OnInit} from '@angular/core';
-import {ThrustersStatusService} from '../../services/publishers/thrusters-status.service';
 import {fromEvent} from 'rxjs';
+import {ThrustersStatusService} from '../../services/publishers/thrusters-status.service';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
+
     icons = [
       {src: '../../../assets/House(Unselected).svg', clickedsrc: '../../../assets/House(Selected).svg', selected: false, link: '/copilot'},
       {src: '../../../assets/Bolt(Unselected).svg', clickedsrc: '../../../assets/Bolt(Selected).svg', selected: false, link: '/drq'},
@@ -15,6 +17,40 @@ export class NavComponent {
       {src: '../../../assets/Settings(Unselected).svg', clickedsrc: '../../../assets/Settings(Selected).svg', selected: false}
     ];
 
+    // Declares variable to hold thruster status (enabled/disabled)
+    thrusterStatus = false;
+    visible = false;
+    partyModevisible = false;
+    audio = new Audio('../../../assets/Party.mp3');
+
+
+    constructor(private thrusterStatusService: ThrustersStatusService, public thrusterNotification: MatSnackBar) {}
+
+    thrustersEnabled() {
+        // Changes thruster status
+        this.thrusterStatus = !this.thrusterStatus;
+        this.thrusterStatusService.publish(this.thrusterStatus);
+        // Opens snackbar (that's the real name) that displays thruster status)
+        this.thrusterNotification.open(this.thrusterStatus ? 'Thrusters Enabled' : 'Thrusters Disabled', 'Exit', {
+            duration: 3000,
+            panelClass: ['snackbar']
+        });
+    }
+
+    //  Runs on key press
+    keyPress(character) {
+        if (character.code === 'Space') {
+            this.thrustersEnabled();
+        } else {
+            console.log(character.code);
+        }
+    }
+
+    ngOnInit() {
+        // Creates and subscribes too observable that listens for keypresses, runs keypress function as callback
+        fromEvent(document, 'keyup').pipe().subscribe(character => this.keyPress(character));
+        this.thrusterStatusService.initialize();
+    }
 
     selected(icon) {
     for (let icon of this.icons) {
@@ -22,10 +58,6 @@ export class NavComponent {
     }
     icon.selected = true;
     }
-
-    visible = false;
-    partyModevisible = false;
-    audio = new Audio('../../../assets/Party.mp3');
 
     openConfirm() {
         this.visible = true;
@@ -46,5 +78,4 @@ export class NavComponent {
         this.audio.currentTime = 0;
         this.partyModevisible = false;
     }
-
 }
