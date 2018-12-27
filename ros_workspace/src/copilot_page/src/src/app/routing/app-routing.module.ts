ad1a10451ad33dcf6110d3530d6914d5a9109fdc
Caelin Sutch <sutchc20@student.jhs.net>
2018-12-27 14:09:17 -0800

    Fixed #72

--- ros_workspace/src/copilot_page/src/src/app/routing/app-routing.module.ts
+++ ros_workspace/src/copilot_page/src/src/app/routing/app-routing.module.ts
@@ -6,6 +6,7 @@ import {DrqComponent} from '../drq/drq.component';
 import {SensortelemetryComponent} from '../sensor-telemetry/sensortelemetry.component';
 
 const appRoutes: Routes = [
+  { path: '', redirectTo: 'copilot' },
   { path: 'drq', component: DrqComponent },
   { path: 'sensor-telemetry', component: SensortelemetryComponent },
   { path: 'copilot', component: CopilotComponent},
