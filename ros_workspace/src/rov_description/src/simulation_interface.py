7ff88143ff1ae490c0850ee421ce9e40685cc78b
Michael Equi <equim20@student.jhs.net>
2019-01-04 11:16:45 -0800

    Updated hydrodynamics and made a more functional model, the visual is not as good: thrusers dont line up with mesh

--- ros_workspace/src/rov_description/src/simulation_interface.py
+++ ros_workspace/src/rov_description/src/simulation_interface.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+from math import sqrt
 from vector_drive.msg import thrusterPercents
 from uuv_gazebo_ros_plugins_msgs.msg import FloatStamped
 
@@ -8,19 +9,39 @@ T1, T2, T3, T4, T5, T6 = 0, 0, 0, 0, 0, 0
 
 rospy.init_node("simulation_interface")
 
+def map(input, inMin, inMax, outMin, outMax):
+    return(((input-inMin)/(inMax-inMin))*(outMax-outMin)+outMin)
+
+##Converts the thruster percent to an rotor angular velocity that can be sent to the basic uuv thruster controller
+# The function assumes a rotor constant of 0.0031 which is the default for the RexROV
+# @param inputVal The input thruster percent*10 from -1000 to 1000
+# @return A propellers angular velocity that can be sent to the uuv simulator thruster manager
+def toRotorAngVel(inputVal):
+    thrusterCmd = map(inputVal, -1000.0, 1000.0, 1100.0, 1900.0) #Convert thruster percent to microseconds signal
+    if thrusterCmd > 1450 and thrusterCmd < 1550: #if the microseconds pulse is in the deadzone
+        return 0
+    else:
+        #cubic function that takes in microseconds and outputs thruster force - source Blue Robotics
+        thrusterForce = (0.000000013299668*(thrusterCmd**3)-0.00005769384*(thrusterCmd**2)+0.0866075898*thrusterCmd-44.91965613) #Thruster Force in Newtons
+        #force is calculated in the uuv simulator with the equation rotorConstant*angularVel^2
+        propAngVel = sqrt(thrusterForce/0.00031*(thrusterForce/abs(thrusterForce)))*(thrusterForce/abs(thrusterForce)) #We know that thrusterForce is not going to be zero here
+        return propAngVel
+
+
+
 #change the sign of the value in oder to compensate for the negative rotor constant
-# rotor constant = -0.00031
+#rotor constant = -0.00031
 def updateVert(data):
     global T1, T2, T3, T4
-    T1 = -data.t1
-    T2 = -data.t2
-    T3 = -data.t3
-    T4 = -data.t4
+    T1 = -toRotorAngVel(data.t1)
+    T2 = -toRotorAngVel(data.t2)
+    T3 = -toRotorAngVel(data.t3)
+    T4 = -toRotorAngVel(data.t4)
 
 def updateHoriz(data):
     global T5, T6
-    T5 = -data.t1
-    T6 = -data.t2
+    T5 = -toRotorAngVel(data.t1)
+    T6 = -toRotorAngVel(data.t2)
 
 
 verticals = rospy.Subscriber("rov/cmd_horizontal_vdrive", thrusterPercents, updateVert)
