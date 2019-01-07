343aed1a598dc184f880e6c3ace6be7b5ccc7c4a
Michael Equi <equim20@student.jhs.net>
2019-01-06 16:04:22 -0800

    Untested DRQ1250 ROS code

--- ros_workspace/src/drq1250/src/drq1250_ros.py
+++ ros_workspace/src/drq1250/src/drq1250_ros.py
@@ -1,24 +1,41 @@
-import time
+#!/usr/bin/env python
+
+import rospy
+from drq1250.msg import DRQ1250
 from PMBus import pmbus
+import sys
 
-print("Initializing PMBUS... \n")
+rospy.init_node("drq2150")
 
-DRQ = pmbus(0x12) #New pmbus object with device address 0x12
+myargv = rospy.myargv(argv=sys.argv)
+if len(myargv) == 2:
+    if myargv[1].startswith("0x"):
+        addr = int(myargv[1][2:], 16)
+    else:
+        addr = int(myargv[1], 16)
+else:
+    rospy.logerr("USAGE WARNING: Please provide an address for the DRQ device! Using default 0x12")
+    addr = 0x12
 
-time.sleep(1)
+rospy.loginfo("Initializing PMBUS... ")
 
-DRQ.setUVLimit(36.0)
+DRQ = pmbus(addr) #New pmbus object with device address 0x12
+rospy.sleep(1)
+DRQ.setUVLimit(36.0) #Not sure if this works yet
+rospy.sleep(1)
 
-time.sleep(1)
+pub = rospy.Publsiher("rov/drq1250", DRQ1250, queue_size=1)
+rate = rospy.Rate(30)
 
-while True:
-    print("Tempurature: " + str(DRQ.getTempurature()))
-    print("Input Voltage: " + str(DRQ.getVoltageIn()))
-    print("Output Voltage: " + str(DRQ.getVoltageOut()))
-    print("Output Current: " + str(DRQ.getCurrent()))
-    print("Output Power: " + str(DRQ.getPowerOut(False)) + "\n\n") #False is caclulated from given values of current and voltage while True gets values from DRQ1250
-    
-    #DRQ.encodePMBus(34.0)
-    
-    time.sleep(5)
+while not rospy.is_shutdown():
+    msg = DRQ1250()
+    msg.header.stamp = rospy.Time.now()
+    msg.header.frame_id = 'drq1250'
+    msg.tempurature = DRQ.getTempurature()
+    msg.Vin = DRQ.getVoltageIn()
+    msg.Vout = DRQ.getVoltageOut()
+    msg.Iout = DRQ.getCurrent()
+    msg.Pout = DRQ.getPowerOut(False) #False is caclulated from given values of current and voltage while True gets values from DRQ1250
 
+    pub.publish(msg)
+    rate.sleep()
